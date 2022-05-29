87342d7d58150181d391c454ba04d53796676e6f
tvgc <thales.v.gc@hotmail.com>
2022-05-29 16:13:57 -0300

    finished project

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,3 +28,13 @@ function linkAction(){
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    distance: '90px',
+    duration: 3000,
+})
+
+sr.reveal(`.home__data`, {origin: 'top, delay: 400'})
+sr.reveal(`.home__img`, {origin: 'bottom', delay: 600})
+sr.reveal(`.home__footer`, {origin: 'bottom', delay: 800})
